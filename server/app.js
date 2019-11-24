dde1bffd726fcba943f0cf8928f19b9fd289f888
mariusparasca <mrs.parasca@gmail.com>
2019-11-24 10:20:14 +0200

    graphql lists, connection to mongo db

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+mongoose.connect("mongodb+srv://marius:telfon25@cluster0-oy4jr.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connection.once('open', () => {
+  console.log("connected to database");
+});
 
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
 }));
 
 app.listen(4000, () => {
