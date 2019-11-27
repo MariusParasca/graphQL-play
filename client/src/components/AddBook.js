ff3beac2ba82c7a5d62b9cb270d21c72cd0f0b14
mariusparasca <mrs.parasca@gmail.com>
2019-11-27 08:46:19 +0200

    compose queries, mutations

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,35 +1,60 @@
-import React, { useCallback } from 'react';
-import { gql } from 'apollo-boost';
+import React, { useCallback, useState } from 'react';
 import { graphql } from 'react-apollo';
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { flowRight as compose } from 'lodash';
+import { getAuthorsQuery, addBookMutation } from '../queries/queries';
 
 function AddBook(props) {
+  const [book, setBook] = useState({ name: "", genre: "", authorId: "" });
 
-  console.log('props', props);
+  const displayAuthors = useCallback(
+    () => {
+      const data = props.getAuthorsQuery;
+      if (data.loading) {
+        return (<option disabled>Loading Authors...</option>);
+      } else {
+        return data.authors.map((author) => (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>));
+      }
+    },
+    [props],
+  );
+
+  const submitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      props.addBookMutation(book);
+    },
+    [book, props],
+  );
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submitForm.bind()}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => {
+          const { value } = e.target;
+          setBook((oldBook) => ({ ...oldBook, name: value }))
+        }} />
       </div>
 
       <div className="field">
         <label>Genre</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => {
+          const { value } = e.target;
+          setBook((oldBook) => ({ ...oldBook, genre: value }))
+        }} />
       </div>
 
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select onChange={(e) => {
+          const { value } = e.target;
+          setBook((oldBook) => ({ ...oldBook, authorId: value }))
+        }}>
           <option>Select author</option>
+          {displayAuthors()}
         </select>
       </div>
 
@@ -38,4 +63,7 @@ function AddBook(props) {
   );
 }
 
-export default graphql(getBooksQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,29 +1,18 @@
 import React, { useCallback } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
 
-const getBooksQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`;
 
 function BookList(props) {
   const displayBooks = useCallback(() => {
-    console.log('props.data', props.data);
     const { data } = props;
     if (data.loading) {
       return <div>Loading books...</div>;
     } else {
-      console.log('data', data);
       return data.books.map(book => (<li key={book.id}>{book.name}</li>));
     }
   }, [props]);
 
-  console.log('props', props);
   return (
     <div>
       <ul id="book-list">
